7c4de59e6e4d236d5fbc467d589b76677601f6c2
chiya morara <chiyamorara@gmail.com>
2025-04-16 17:30:34 +0300

    edited css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -60,7 +60,7 @@ const Home = () => {
       
   return (
     <>
-    <div id="parentDiv">
+    <div id="ParentDiv">
         <AddExpenseForm  handleSubmit={handleSubmit} handleChange={handleChange} newExpense={newExpense}/>   
         <div id="tableAndSearch">  
         <Search searchTerm ={searchTerm}  handleSearchTerm ={handleSearchTerm} handleOnchangeSearch={handleOnchangeSearch}/>
